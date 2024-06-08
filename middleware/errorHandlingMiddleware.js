f1a12f37e08d4ec3b08eeebdc120495f05aff89a
wDevCristian <cristian.gurduza@gmail.com>
2024-06-08 17:55:39 +0300

    fix(middleware): fix middleware sending respons header after the body

--- middleware/errorHandlingMiddleware.js
+++ middleware/errorHandlingMiddleware.js
@@ -3,7 +3,7 @@ import ApiError from "../error/apiError.js";
 export function errorHandlingMiddleware(err, req, res, next) {
   if (err instanceof ApiError) {
     res.status(err.status).json(err.message);
+  } else {
+    res.status(500).json({ message: `Unhandled error:  ${err.message}` });
   }
-
-  res.status(500).json({ message: `Unhandled error:  ${err.message}` });
 }
